f33890dc04e409896116eb12fec2875ed882e47b
Michael Liu <lium039@mgail.com>
2016-11-15 13:25:56 -0800

    sorting client server

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cookieSession({
-  name: 'trackify',
+  name: 'vrExperience',
   secret: process.env.SESSION_SECRET
 }));
 
